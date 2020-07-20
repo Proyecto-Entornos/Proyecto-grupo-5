1943081ade797c28cd9f8e94db5249613ef04fe4
Melva_Romero <65907669+Melva-99@users.noreply.github.com>
2020-07-20 08:53:51 -0600

    Add files via upload

---
+++ Boleteria/Boleteria/Conexion.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class Conexion
+    Public conexion As SqlConnection = New SqlConnection("Data Source= DESKTOP-IKI4BG5\MSSQLSERVER01;Initial Catalog=; Integrated Security=True")
+    Private cmb As SqlCommandBuilder
+    Public ds As DataSet = New DataSet()
+    Public da As SqlDataAdapter
+    Public comand As SqlCommand
+    Public dv As New DataView
+    Public adap As SqlDataAdapter
+    Public datos2 As DataSet
+    Public cmd As New SqlCommand
+
+    Public Sub conectar()
+        Try
+            conexion.Open()
+            MessageBox.Show("Conexión exitosa")
+        Catch ex As Exception
+            MessageBox.Show("Error conexion de base de datos")
+        Finally
+            conexion.Close()
+        End Try
+    End Sub
+    Public Sub Llenar(ByVal sql, ByVal tabla)
+        ds.Tables.Clear()
+        da = New SqlDataAdapter(sql, conexion)
+        cmb = New SqlCommandBuilder(da)
+        da.Fill(ds, tabla)
+        dv.Table = ds.Tables(0)
+    End Sub
+    Public Function ingresarBus(matricula As String, marca As String, modelo As String, NoAsientos As Integer, añosServicio As Integer, kilometraje As Integer, color As String, estado As String, conductor As String, ayudante As String)
+        Try
+            conexion.Open()
+            comand = New SqlCommand("agregarAutobus", conexion)
+            comand.CommandType = CommandType.StoredProcedure
+            comand.Parameters.AddWithValue("@matricula", matricula)
+            comand.Parameters.AddWithValue("@marca", marca)
+            comand.Parameters.AddWithValue("@modelo", modelo)
+            comand.Parameters.AddWithValue("@NoAsientos", NoAsientos)
+            comand.Parameters.AddWithValue("@añosServicio", añosServicio)
+            comand.Parameters.AddWithValue("@kilometraje", kilometraje)
+            comand.Parameters.AddWithValue("@color", color)
+            comand.Parameters.AddWithValue("@estado", estado)
+            comand.Parameters.AddWithValue("@conductorAsignado", conductor)
+            comand.Parameters.AddWithValue("@ayudanteAsignado", ayudante)
+            If comand.ExecuteNonQuery Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Function
+
+    Public Function modificarBus(matricula As String, marca As String, modelo As String, NoAsientos As Integer, añosServicio As Integer, kilometraje As Integer, color As String, estado As String, conductor As String, ayudante As String)
+        Try
+            conexion.Open()
+            comand = New SqlCommand("actualizarAutobus", conexion)
+            comand.CommandType = CommandType.StoredProcedure
+            comand.Parameters.AddWithValue("@matricula", matricula)
+            comand.Parameters.AddWithValue("@marca", marca)
+            comand.Parameters.AddWithValue("@modelo", modelo)
+            comand.Parameters.AddWithValue("@NoAsientos", NoAsientos)
+            comand.Parameters.AddWithValue("@añosServicio", añosServicio)
+            comand.Parameters.AddWithValue("@kilometraje", kilometraje)
+            comand.Parameters.AddWithValue("@color", color)
+            comand.Parameters.AddWithValue("@estado", estado)
+            comand.Parameters.AddWithValue("@conductorAsignado", conductor)
+            comand.Parameters.AddWithValue("@ayudanteAsignado", ayudante)
+            If comand.ExecuteNonQuery Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Function
+
+    Function eliminarBus(ByVal matricula As String)
+        Dim sqlComm As SqlCommand = conexion.CreateCommand()
+        sqlComm.CommandText = "eliminarRegistro"
+        sqlComm.CommandType = CommandType.StoredProcedure
+        sqlComm.Parameters.AddWithValue("@matricula", matricula)
+        conexion.Open()
+        If sqlComm.ExecuteNonQuery() Then
+            conexion.Close()
+            Return True
+        Else
+            conexion.Close()
+            Return False
+        End If
+
+    End Function
+End Class
---
+++ Boleteria/Boleteria/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Dim Conexion As New Conexion
+    Dim NoAsientos, añosServicio, kilometraje As Integer
+    Dim matricula, marca, modelo, color, estado, conductor, ayudante As String
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        If Conexion.eliminarBus(txtMatricula.Text) Then
+            MsgBox("ELIMINADO", vbInformation)
+            Conexion.Llenar("Select id as 'ID', matricula as 'Matricula', marca as 'Marca', modelo as 'Modelo', NoAsientos as 'No. Asientos', añosServicio as 'Años de servicio', kilometraje as 'Kilométraje', color as 'Color', estado as 'Estado', conductorAsignado as 'Conductor', ayudanteAsignado as 'Ayudante' from autobuses where matricula = '" & txtMatricula.Text & "'", "autobuses")
+            DataBus.DataSource = Conexion.ds.Tables("autobuses")
+            Limpiar()
+        Else
+            MsgBox("ERROR INESPERADO", vbCritical)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Conexion.conectar()
+        Llenar()
+    End Sub
+    Private Sub Llenar()
+        Conexion.Llenar("Select id as 'ID', matricula as 'Matricula', marca as 'Marca', modelo as 'Modelo', NoAsientos as 'No. Asientos', añosServicio as 'Años de servicio', kilometraje as 'Kilométraje', color as 'Color', estado as 'Estado', conductorAsignado as 'Conductor', ayudanteAsignado as 'Ayudante' from autobuses", "autobuses")
+        DataBus.DataSource = Conexion.ds.Tables("autobuses")
+    End Sub
+    Private Sub agregarAutobus()
+        matricula = txtMatricula.Text
+        marca = txtMarca.Text
+        modelo = txtModelo.Text
+        NoAsientos = txtAsiento.Text
+        añosServicio = txtAño.Text
+        kilometraje = txtKM.Text
+        Color = cmbColor.Text
+        estado = cmbEstado.Text
+        conductor = cmbConductor.Text
+        ayudante = cmbAyudante.Text
+        Try
+            If Conexion.ingresarBus(matricula, marca, modelo, NoAsientos, añosServicio, kilometraje, Color, estado, conductor, ayudante) Then
+                MessageBox.Show("Guardado", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Error al guardar", "Incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        agregarAutobus()
+    End Sub
+
+    Private Sub DataBus_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataBus.CellClick
+        Dim FilaActual As Integer
+        FilaActual = DataBus.CurrentRow.Index
+        txtMatricula.Text = DataBus.Rows(FilaActual).Cells(1).Value
+        txtMarca.Text = DataBus.Rows(FilaActual).Cells(2).Value
+        txtModelo.Text = DataBus.Rows(FilaActual).Cells(3).Value
+        txtAsiento.Text = DataBus.Rows(FilaActual).Cells(4).Value
+        txtAño.Text = DataBus.Rows(FilaActual).Cells(5).Value
+        txtKM.Text = DataBus.Rows(FilaActual).Cells(6).Value
+        cmbColor.Text = DataBus.Rows(FilaActual).Cells(7).Value
+        cmbEstado.Text = DataBus.Rows(FilaActual).Cells(8).Value
+        cmbConductor.Text = DataBus.Rows(FilaActual).Cells(9).Value
+        cmbAyudante.Text = DataBus.Rows(FilaActual).Cells(10).Value
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        matricula = txtMatricula.Text
+        marca = txtMarca.Text
+        modelo = txtModelo.Text
+        NoAsientos = txtAsiento.Text
+        añosServicio = txtAño.Text
+        kilometraje = txtKM.Text
+        color = cmbColor.Text
+        estado = cmbEstado.Text
+        conductor = cmbConductor.Text
+        ayudante = cmbAyudante.Text
+        Try
+            If Conexion.modificarBus(matricula, marca, modelo, NoAsientos, añosServicio, kilometraje, color, estado, conductor, ayudante) Then
+                MsgBox("Datos Actualizados", vbInformation)
+                Limpiar()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub Limpiar()
+        txtMatricula.Clear()
+        txtMarca.Clear()
+        txtModelo.Clear()
+        txtAsiento.Clear()
+        txtAño.Clear()
+        txtKM.Clear()
+        cmbColor.SelectedIndex = 0
+        cmbEstado.SelectedIndex = 0
+        cmbConductor.SelectedIndex = 0
+        cmbAyudante.SelectedIndex = 0
+    End Sub
+End Class
